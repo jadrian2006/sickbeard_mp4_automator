d0ed5ba0a945df01adb29577953ea53702c549a9
Justin Adrian <jadrian2006@gmail.com>
2019-01-29 19:17:59 -0500

    rabbit_consumer v.01 done

--- rabbit_consumer.py
+++ rabbit_consumer.py
@@ -1,15 +1,38 @@
 #!/usr/bin/env python
 
+import threading
 import pika
+import os
+import sys
+import json
 from autoprocess import autoProcessTV, autoProcessMovie, autoProcessTVSR, sonarr, radarr
 from readSettings import ReadSettings
+from mkvtomp4 import MkvtoMp4
+from deluge_client import DelugeRPCClient
 import logging
 from logging.config import fileConfig
+import delugePostProcess as delugePost
+
+logpath = './logs/rmq'
+if os.name == 'nt':
+    logpath = os.path.dirname(sys.argv[0])
+elif not os.path.isdir(logpath):
+    try:
+        os.mkdir(logpath)
+    except:
+        logpath = os.path.dirname(sys.argv[0])
+configPath = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]), 'logging.ini')).replace("\\", "\\\\")
+logPath = os.path.abspath(os.path.join(logpath, 'index.log')).replace("\\", "\\\\")
+fileConfig(configPath, defaults={'logfilename': logPath})
+log = logging.getLogger("rabbit_consumer")
+
+log.info("Rabbit Consumer Log Started.")
 
 class rabbitConsumer(object):
     EXCHANGE = 'message'
     EXCHANGE_TYPE = 'topic'
     QUEUE = 'task_queue'
+    DURABLE = 'True'
     ROUTING_KEY = 'task_queue'
 
     def __init__(self, amqp_url):
@@ -69,11 +92,11 @@ class rabbitConsumer(object):
 
     def on_exchange_declareok(self, unused_frame):
         log.info('Exchange declared')
-        self.setup_queue(self.QUEUE)
+        self.setup_queue(self.QUEUE, self.DURABLE)
         
-    def setup_queue(self, queue_name):
+    def setup_queue(self, queue_name, queue_durable):
         log.info('Declaring queue %s', queue_name)
-        self._channel.queue_declare(self.on_queue_declareok, queue_name)
+        self._channel.queue_declare(self.on_queue_declareok, queue_name, queue_durable)
 
     def on_queue_declareok(self, method_frame):
         log.info('Binding %s to %s with %s', self.EXCHANGE, self.QUEUE, self.ROUTING_KEY)
@@ -92,9 +115,20 @@ class rabbitConsumer(object):
         log.info('Acknowledging message %s', delivery_tag)
         self._channel.basic_ack(delivery_tag)
 
+    def t_spawn_on_message(self, body, delivery_tag):
+        log.info('Worker thread started with %s', body)
+        log.debug('Delivery Tag information %s', delivery_tag)
+        body = json.loads(body)
+        parms = ["",body[1]['deluge']['torrent_id'],body[1]['deluge']['torrent_name'],body[1]['deluge']['path']]
+        delugePost.main(parms)
+        self.acknowledge_message(delivery_tag)
+
+
     def on_message(self, unused_channel, basic_deliver, properties, body):
         log.info('Received message # %s from %s: %s', basic_deliver.delivery_tag, properties.app_id, body)
-        self.acknowledge_message(basic_deliver.delivery_tag)
+        t = threading.Thread(target=self.t_spawn_on_message, args=(body, basic_deliver.delivery_tag,))
+        t.start()
+        threads.append(t)
 
     def on_cancelok(self, unused_frame):
         log.info('RabbitMQ acknowledged the cancellation of the consumer')
@@ -108,6 +142,7 @@ class rabbitConsumer(object):
     def start_consuming(self):
         log.info('Issuing consumer related RPC commands')
         self.add_on_cancel_callback()
+        self._channel.basic_qos(prefetch_count=1)
         self._consumer_tag = self._channel.basic_consume(self.on_message, self.QUEUE)
 
     def on_bindok(self, unused_frame):
@@ -133,12 +168,16 @@ class rabbitConsumer(object):
         self._connection.ioloop.start()
         log.info('Stopped')
 
+threads = []
+
 def main():
     example = rabbitConsumer('amqp://guest:guest@172.16.100.11:5672/%2F')
     try:
         example.run()
+        
     except KeyboardInterrupt:
         example.stop()
+
 
 if __name__ == '__main__':
     main()
